8cdbfb5a30c7f3d14b5e96cd4c8a08f6df88d890
Reynaldo Putra <reynaldo.informatic@engineer.com>
2023-11-06 09:09:15 +0700

    add readme.md, update tailwind.config.js, update app.js, update vite.config.js

---
+++ README.md
@@ -0,0 +1,24 @@
+# SIRUS
+
+SIRUS V2.0 (Laravel 10 + Jetstream 4 + livewire 3)
+
+<p align="left">
+<a href="https://github.com/laravel/framework/actions"><img src="https://github.com/laravel/framework/workflows/tests/badge.svg" alt="Build Status"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
+</p>
+
+## About SIRUS
+
+SIRUS (Sinar Roda Utama Surabaya) app is a web application build with laravel framework, dashboard admin using jetstream version 4 (with custom theme), and tailwind version 3.
+
+## Contributing
+
+Thank you for considering contributing to the SIRUS app! To contribution you can send an e-mail to Reynaldol via [reynaldo.informatic@engineer.com](mailto:reynaldo.informatic@engineer.com).
+
+## Security Vulnerabilities
+
+If you discover a security vulnerability within SIRUS, please send an e-mail to Reynaldol via [reynaldo.informatic@engineer.com](mailto:reynaldo.informatic@engineer.com). All security vulnerabilities will be promptly addressed.
+
+## License
+
+The SIRUS app is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,47 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "class",
+
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+
+            colors: {
+                dark: {
+                    "eval-0": "#151823",
+                    "eval-1": "#222738",
+                    "eval-2": "#2A2F42",
+                    "eval-3": "#2C3142",
+                },
+                cyan: colors.cyan,
+                //Custom
+                disable: {
+                    light: "#E9ECEF",
+                    dark: "#2C3142",
+                },
             },
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [
+        require("@tailwindcss/forms")({
+            strategy: "class",
+        }),
+        typography,
+    ],
 };
