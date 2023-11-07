21d32c2120d1d385c1de2265c12d6ead57b7ac06
Reynaldo Putra <reynaldo.informatic@engineer.com>
2023-11-07 10:28:42 +0700

    update confirmation-modal, confirms-password, dialog-modal, dropdown-link, dropdown, form-section, input template components

--- resources/views/components/confirms-password.blade.php
+++ resources/views/components/confirms-password.blade.php
@@ -24,7 +24,7 @@
         {{ $content }}
 
         <div class="mt-4" x-data="{}" x-on:confirming-password.window="setTimeout(() => $refs.confirmable_password.focus(), 250)">
-            <x-input type="password" class="mt-1 block w-3/4" placeholder="{{ __('Password') }}" autocomplete="current-password"
+            <x-input type="password" id="confirmPassword" name="confirmPassword" class="form-input mt-1 block w-3/4" placeholder="{{ __('Password') }}" autocomplete="off"
                         x-ref="confirmable_password"
                         wire:model="confirmablePassword"
                         wire:keydown.enter="confirmPassword" />
@@ -34,11 +34,11 @@
     </x-slot>
 
     <x-slot name="footer">
-        <x-secondary-button wire:click="stopConfirmingPassword" wire:loading.attr="disabled">
+        <x-button variant="info" wire:click="stopConfirmingPassword" wire:loading.attr="disabled">
             {{ __('Cancel') }}
-        </x-secondary-button>
+        </x-button>
 
-        <x-button class="ms-3" dusk="confirm-password-button" wire:click="confirmPassword" wire:loading.attr="disabled">
+        <x-button class="ml-3" dusk="confirm-password-button" wire:click="confirmPassword" wire:loading.attr="disabled">
             {{ $button }}
         </x-button>
     </x-slot>
