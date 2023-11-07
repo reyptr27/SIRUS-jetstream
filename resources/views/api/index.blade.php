f059337f43984f08f4c2690c9350f4f860f5aab1
Reynaldo Putra <reynaldo.informatic@engineer.com>
2023-11-07 08:19:07 +0700

    fix bug api-index

--- resources/views/api/index.blade.php
+++ resources/views/api/index.blade.php
@@ -1,12 +1,6 @@
 <x-app-layout>
 
     @section('title', __('Create API Token'))
-    
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('API Tokens') }}
-        </h2>
-    </x-slot> --}}
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl leading-tight">
