e63bc4b60fced466ede5c0d1e47a17c944bf4c0c
Reynaldo Putra <reynaldo.informatic@engineer.com>
2023-11-07 10:14:25 +0700

    update action-section, app-logo, banner, checkbox form template

--- resources/views/components/action-section.blade.php
+++ resources/views/components/action-section.blade.php
@@ -1,11 +1,11 @@
-<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
+<div class="grid gap-4 md:gap-6" {{ $attributes }}>
     <x-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
     </x-section-title>
 
     <div class="mt-5 md:mt-0 md:col-span-2">
-        <div class="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <div class="p-4 bg-white shadow-md rounded-md sm:p-6 dark:bg-dark-eval-1">
             {{ $content }}
         </div>
     </div>
